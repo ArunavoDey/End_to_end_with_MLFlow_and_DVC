5cddab9a93f8ccbeafe36beba078c52af08795c6
ArunavoDey <arunavo071@gmail.com>
2024-08-24 14:54:33 -0500

    Adding datat ingestion stage

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 from cnnClassifier import logger
+from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Welcome to cnnClassifier")
+STAGE_NAME = "data_ingestion_stage"
+if __name__=="__main__":
+    try:
+        logger.info("Welcome to cnnClassifier")
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f">>>> stage {STAGE_NAME} completed <<<<<\n\nx========x\n")
+    except Exception as e:
+        logger.exception(e)
+        raise e
